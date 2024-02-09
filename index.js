b04111c020a9522c0b453c90700d81ff33ca62d2
Sasha <sasha@2437047-bl90648.twc1.net>
2024-02-09 16:08:50 +0300

    feat: changed config

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
 var bodyParser = require("body-parser");
 
-connectDB(); // connect database
+// connectDB();
 
 // initialize app
 const app = express();
@@ -17,8 +17,8 @@ app.use(bodyParser.urlencoded({ limit: "16mb", extended: true }));
 app.use("/api/v1/auth", require("./routes/auth"));
 
 // initialize tg bot
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.API_KEY_BOT, { polling: true });
+// const TelegramBot = require('node-telegram-bot-api');
+// const bot = new TelegramBot(process.env.API_KEY_BOT, { polling: true });
 
 // Error Handler Middleware
 const errorHandler = require("./middleware/error");
@@ -35,15 +35,15 @@ process.on("unhandledRejection", (err, promise) => {
   server.close(() => process.exit(1));
 });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  // const chatId = msg.chat.id;
+  // const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+  // bot.sendMessage(chatId, resp);
+// });
 
